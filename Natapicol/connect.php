f3a62aea88eee6e5fb65c93aa090a107d3f812ff
IanCapulong <iancaps.101@gmail.com>
2024-08-08 15:53:35 +0800

    Add files via upload

---
+++ Natapicol/connect.php
@@ -0,0 +1,15 @@
+
+<?php
+/*
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "id20935514_root";
+ $dbpass = "B7QWaO7&BQmvR@1mJr^V";
+ $db = "id20935514_barangay";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ */
+ ?>
